6cb9e95d6f2636dfe7678aa515f9e2c432906caa
thanhnhan591 <68978317+thanhnhan591@users.noreply.github.com>
2020-09-28 22:58:32 +0700

    Add files via upload

---
+++ contact/main.js
@@ -0,0 +1,38 @@
+//ShowInfo
+/*function showInfo() 
+{
+	var fcat = document.getElementById("fcat").value;
+    var ftitle = document.getElementById("ftitle").value;
+    var name = document.getElementById("fname").value;
+    var email = document.getElementById("femail").value;
+    var phone = document.getElementById("fphone").value;
+    var comment = document.getElementById("fcon").value;
+
+    alert ("FEEDBACK INFORMATION" + "\n" +
+        "-----------------------" + "\n" +
+        fcat + "\n" +
+        ftitle + "\n" +
+        name + "\n" +
+        email + "\n" +
+        phone + "\n" +
+        comment + "\n"
+     );
+}*/
+
+function showInfo() 
+{
+    var ftitle = document.getElementById("ftitle").value;
+    var fname = document.getElementById("fname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var comment = document.getElementById("comment").value;
+
+    alert ("THANK YOU FOR FEEDBACK INFORMATION" + "\n" +
+        "-----------------------" + "\n" +
+        ftitle + "\n" +
+        fname + "\n" +
+        email + "\n" +
+        phone + "\n" +
+        comment + "\n"
+     );
+}
